08fe1ab7edbab8904cb4858b85bf1e7d9801246b
Talia18 <taliavaknnin@gmail.com>
2022-09-11 21:08:29 +0300

    homework7

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let firstNumber = Number(prompt("הזן מספר"));
-let secondNumber = Number(prompt("הזן מספר שני"));
+let result;
+let firstNumber;
+let secondNumber;
+
+firstNumber = Number(prompt("הזן מספר"));
+
+while (isNaN(firstNumber)) {
+  alert("הערך הראשון אינו מספר");
+  firstNumber = Number(prompt("הזן מספר"));
+}
+
 let operator = prompt(
   'לחיבור הזן "+" לחיסור הזן "-" לכפל הזן "*" לחילוק הזן "/"'
 );
 
-if (isNaN(firstNumber)) {
-  alert("הערך הראשון אינו מספר");
-} else if (isNaN(secondNumber)) {
-  alert("הערך השני אינו מספר");
-} else {
-  let result;
-  if (operator == "+") {
-    result = firstNumber + secondNumber;
-  } else if (operator == "-") {
-    result = firstNumber - secondNumber;
-  } else if (operator == "*") {
-    result = firstNumber * secondNumber;
-  } else if (operator == "+") {
-    result = firstNumber / secondNumber;
-  } else {
-    result = "נא להזין מספר תקני";
-  }
-
-  alert(result);
+let oper = ["+", "-", "*", "/"];
+if (!oper.includes(operator)) {
+  alert("הזנת אופרציה שגויה!");
 }
+secondNumber = Number(prompt("הזן מספר שני"));
+
+while (isNaN(secondNumber)) {
+  alert("הערך השני אינו מספר");
+  secondNumber = Number(prompt("הזן מספר"));
+}
+
+if (operator == "+") {
+  result = firstNumber + secondNumber;
+} else if (operator == "-") {
+  result = firstNumber - secondNumber;
+} else if (operator == "*") {
+  result = firstNumber * secondNumber;
+} else if (operator == "+") {
+  result = firstNumber / secondNumber;
+}
+
+alert(result);
